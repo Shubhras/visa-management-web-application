78acc672df433c5ebdec21feb3c38e42031bd418
hp <kshitij>
2025-10-25 15:13:53 +0530

    new changes

--- visamanagement/master/models.py
+++ visamanagement/master/models.py
@@ -132,16 +132,15 @@ class Timezone(models.Model):
 
 
 class Department(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, unique=True, verbose_name="Department Name", help_text="Name of the department")
-    description = models.TextField(max_length=255, blank=True, verbose_name="Description", help_text="Optional description of the department")
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
-
+    class Meta:
+        unique_together = ('name', 'is_deleted')  # enforce uniqueness only among active records
 
 class EmployeeType(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
