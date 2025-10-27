5ee98f3e6b05ccdda66b74554d24be49af5c9a0f
ghanshyam <ghanshyam.nagar@digiprima.com>
2025-10-27 12:37:02 +0530

    changes

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,15 +7,15 @@ module.exports = {
       watch: true,             // Watches files in development
       env: {
         NODE_ENV: "development",
-        PORT: 3000,            // Dev port
+        PORT: 3001,            // Dev port
       },
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000,            // Production port
-        // Use "serve -s build" to serve the build folder
-        SCRIPT: "npx",
-        ARGS: "serve -s build -l 3000"
-      },
+      // env_production: {
+      //   NODE_ENV: "production",
+      //   PORT: 3001,            // Production port
+      //   // Use "serve -s build" to serve the build folder
+      //   SCRIPT: "npx",
+      //   ARGS: "serve -s build -l 3001"
+      // },
     },
     ],
   };
