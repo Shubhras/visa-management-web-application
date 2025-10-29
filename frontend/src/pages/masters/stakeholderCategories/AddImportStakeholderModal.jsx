326863e77d385b3d80923cc438244969ab417689
ghanshyam <ghanshyam.nagar@digiprima.com>
2025-10-29 18:34:10 +0530

    changes

--- frontend/src/pages/masters/stakeholderCategories/AddImportStakeholderModal.jsx
+++ frontend/src/pages/masters/stakeholderCategories/AddImportStakeholderModal.jsx
@@ -109,7 +109,7 @@ const AddImportStakeholderModal = ({ show, handleClose }) => {
         setLoading(false);
     };
     const handleDownloadSample = () => {
-        const fileUrl = 'assets/simplefile/stakeholder.xlsx'; // Update this path according to your project structure
+        const fileUrl = 'assets/simplefile/stakeholderCategories.xlsx'; // Update this path according to your project structure
 
         const link = document.createElement('a');
         link.href = fileUrl;
