a9320d10eb09bdc30d88700679a6c657758731f1
hp <kshitij>
2025-11-01 18:31:53 +0530

    new chgnes

--- visamanagement/master/views.py
+++ visamanagement/master/views.py
@@ -8830,7 +8830,7 @@ class LostReasonB2BExportAPIView(APIView):
 
         field_header_map = {
             'uuid': 'UUID',
-            'name': 'Last Reason (B2B)',  
+            'name': 'Lost Reason (B2B)',  
             'description': 'Description',
             'is_deleted': 'Deleted',
             'updated_at': 'Modified On',
