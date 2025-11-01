41fcce57b1f6ca19562ec05c031d95b0bbff8119
hp <kshitij>
2025-11-01 18:07:01 +0530

    new chgnes

--- visamanagement/master/views.py
+++ visamanagement/master/views.py
@@ -8906,7 +8906,7 @@ class LostReasonB2BImportAPIView(APIView):
         duplicate_names = []
         data = []
 
-        required_headers = {'lost reason (b2b)'}
+        required_headers = {'lost reason b2b'}
         optional_headers = {'description'}
 
         if not file:
