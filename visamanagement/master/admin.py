ce99dd8c5e9c7ac9f034a8d11507e3173212b3f0
hp <kshitij>
2025-11-04 13:12:14 +0530

    new changes

--- visamanagement/master/admin.py
+++ visamanagement/master/admin.py
@@ -227,7 +227,7 @@ class EducationLevelCodeAdmin(admin.ModelAdmin):
 
 @admin.register(EducationLevel)
 class EducationLevelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'is_deleted', 'created_at', 'updated_at')
+    list_display = ('level_code', 'description', 'is_deleted', 'created_at', 'updated_at')
     search_fields = ('level_code__level_code',)
     list_filter = ('is_deleted',)
     ordering = ('level_code__level_code',)
