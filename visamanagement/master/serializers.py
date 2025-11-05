86a1917bcb466aaba1e074032226c287b4b35f12
hp <kshitij>
2025-11-05 13:14:05 +0530

    new chnges

--- visamanagement/master/serializers.py
+++ visamanagement/master/serializers.py
@@ -73,7 +73,7 @@ class CountrySerializer(serializers.ModelSerializer):
         model = Country
         fields = [
             'uuid', 'name', 'continent', 'continent_id',
-            'currencyfullname','currencyshortname','description'
+            'currencyfullname','currencyshortname','description',
             'shortName', 'fullName', 'officialName', 'capitalCity',
             'dialCodes', 'currencyCode', 'status',
             'is_active', 'is_deleted', 'created_at', 'updated_at'
