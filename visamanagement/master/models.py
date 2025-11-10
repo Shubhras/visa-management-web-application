5f665c88f25353783c46d6b9f87fde8179616049
hp <kshitij>
2025-11-10 15:15:09 +0530

    new chgnes

--- visamanagement/master/models.py
+++ visamanagement/master/models.py
@@ -17,7 +17,7 @@ class Gender(models.Model):
 class Maritalstatus(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(max_length=255,null=True, blank=True),
+    description = models.TextField(max_length=255,null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
