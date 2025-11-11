32f0a91203f8c8a18bf405e23a777311cba10c4b
hp <kshitij>
2025-11-11 11:51:09 +0530

    new chgnes

--- visamanagement/master/views.py
+++ visamanagement/master/views.py
@@ -7508,7 +7508,7 @@ class AccreditationNameImportAPIView(APIView):
         optional_headers = {
             'accrediation short name',
             'accrediation issuing authority name',
-            'accrediation valid type',
+            'accrediation valid upto',
             'accrediation valid duration value',
             'accrediation valid duration unit',
             'accrediation valid date',
@@ -7589,7 +7589,7 @@ class AccreditationNameImportAPIView(APIView):
                 short_name = str(row.get('accrediation short name')).strip() if row.get('accrediation short name') else ''
                 issuing_authority = str(row.get('accrediation issuing authority name')).strip() if row.get('accrediation issuing authority name') else ''
                 description = str(row.get('description')).strip() if row.get('description') else ''
-                valid_type = str(row.get('accrediation valid type')).strip() if row.get('accrediation valid type') else None
+                valid_type = str(row.get('accrediation valid upto')).strip() if row.get('accrediation valid type') else None
                 valid_duration_value = row.get('accrediation valid duration value')
                 valid_duration_unit = str(row.get('accrediation valid duration unit')).strip().upper() if row.get('accrediation valid duration unit') else None
                 valid_date_raw = row.get('accrediation valid date')
