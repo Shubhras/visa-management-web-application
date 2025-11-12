3d1841adc4c4be2901623d0439a3cb30a3a94452
hp <kshitij>
2025-11-12 14:44:05 +0530

    new changes

--- visamanagement/master/models.py
+++ visamanagement/master/models.py
@@ -166,11 +166,14 @@ class Timezone(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     countryName=models.ForeignKey(Country,on_delete=models.SET_NULL,related_name="timezone", blank=True, null=True)
     stateName=models.ForeignKey(State,on_delete=models.SET_NULL,related_name="timezone", blank=True, null=True)
-    Timezone =models.CharField(max_length=255, unique=True)
+    Timezone =models.CharField(max_length=255)
     description = models.TextField(max_length=255,null=True,blank=True)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)  
     updated_at = models.DateTimeField(auto_now=True) 
+
+    class Meta:
+        unique_together = ('Timezone', 'countryName')
 
     def __str__(self):
         return self.Timezone
