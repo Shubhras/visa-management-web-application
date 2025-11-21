b68afed749693140aea031a3f5c8575a979175e6
ghanshyam <ghanshyam.nagar@digiprima.com>
2025-11-21 10:36:57 +0530

    changes

--- frontend/src/masterLayout/MasterLayout.jsx
+++ frontend/src/masterLayout/MasterLayout.jsx
@@ -100,7 +100,7 @@ const [searchAllowedModules] = useState([
             { name: "Ownership Type", path: "/ownership-type" },
             { name: "Stakeholder Category", path: "/stakeholder-list" },
             { name: "Stakeholder Type", path: "/stakeholder-type" },
-            { name: "Accrediation Category ", path: "/accrediation-category" },
+            { name: "Accrediation Category", path: "/accrediation-category" },
             { name: "Accrediation Name", path: "/accrediation-name" },
             { name: "Bank Account Type", path: "/bank-ccount-type" },
             { name: "License Name", path: "/license-name" },
