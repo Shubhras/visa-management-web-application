d8ad8a294eb209fff46a020d0cd9f2e14328c5f1
ravitiwari96 <ravi505tiwarigmail.com>
2025-11-26 16:03:44 +0530

    push by ravi]

--- visamanagement/master/educationmaster.py
+++ visamanagement/master/educationmaster.py
@@ -1215,6 +1215,7 @@ class EducationLevelExportAPIView(APIView):
             sort_field_map = {
                 'uuid': 'uuid',
                 'educationLevelCode': 'level_code__name',  # FK sorting key ✅
+                'educationlevelCode': 'level_code__name',  # FK sorting key ✅
                 'level_code': 'level_code__name',
                 'educationlevel': 'educationlevel',
                 'duration': 'durations',   # User sends "duration" but model has "durations" ✅
