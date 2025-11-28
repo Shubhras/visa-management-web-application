57090a491f7791b22f5e83c133134a5c5bcb505f
ravitiwari96 <ravi505tiwarigmail.com>
2025-11-28 11:59:31 +0530

    push by ravi on friday

--- visamanagement/master/models.py
+++ visamanagement/master/models.py
@@ -662,8 +662,8 @@ class ECAAwardingBody(models.Model):
     )
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    country = models.ForeignKey("Country", on_delete=models.SET_NULL, related_name="ECAAwarding_body", blank=True, null=True)
-    ecafor=models.ForeignKey("ECAFor", on_delete=models.SET_NULL, related_name="ECAAwarding_body", blank=True, null=True)
+    country = models.ForeignKey("Country", on_delete=models.SET_NULL, related_name="Country_ECAAwarding_body", blank=True, null=True)
+    ecafor=models.ForeignKey("ECAFor", to_field="uuid", db_column="ecafor_id", on_delete=models.SET_NULL, related_name="ECAFor_ECAAwarding_body", blank=True, null=True)
     valid_duration_value = models.IntegerField(blank=True, null=True)
     eca_body_full_name = models.CharField(max_length=255,blank=True, null=True)
     description = models.TextField(blank=True, null=True)
